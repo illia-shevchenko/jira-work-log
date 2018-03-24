87a979a6eaebac5a276fe08c7d2d08ee80379b86
Illia Shevchenko <illyashevchenko@gmail.com>
2018-03-24 19:43:07 +0200

    (server) Reconsider static file sending mechanism to support SPA

--- server/app.js
+++ server/app.js
@@ -2,7 +2,6 @@
 
 const config = require('../config');
 const Koa = require('koa');
-const serve = require('koa-static');
 const session = require('koa-session');
 /**
  * @type {Application}
@@ -28,9 +27,6 @@ if (!config.env.isTest) {
 }
 
 app.use(error());
-
-console.log('Start static: ', config.client.path);
-app.use(serve(config.client.path));
 
 app.use(session(config.session, app));
 
--- server/routes/index.js
+++ server/routes/index.js
@@ -3,10 +3,27 @@
 /** @type {Router} */
 const api = require('./api');
 const Router = require('koa-router');
+const send = require('koa-send');
+
+const { client: { path, outFile } } = require('../../config');
 
 /** @type {Router} */
 const router = new Router();
 
 router.use('/api', api.routes());
+
+console.log('Start static: ', path);
+
+router.get('*', async (context) => {
+  try {
+    await send(context, context.path, { index: outFile, root: path });
+  } catch (error) {
+    if (error.statusCode === 404) {
+      await send(context, outFile, { root: path });
+    } else {
+      throw error;
+    }
+  }
+});
 
 module.exports = router;
