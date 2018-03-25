2802069a0c4af60e414c5b7d3abdb01f13b2bbc1
Illia Shevchenko <illyashevchenko@gmail.com>
2018-03-25 14:38:29 +0300

    (client) Add user api resource

--- client/api/resources.js
+++ client/api/resources.js
@@ -1,4 +1,7 @@
-import { post } from './call-api';
+import { post, get } from './call-api';
 
 export const login = (username, password) =>
   post('login', { username, password });
+
+export const queryUsers = ({ usernames, dateFrom, dateTo }) =>
+  get('worklog', { usernames, dateFrom, dateTo });
