1fe7d4c16f5e43dafd72db19e2a7a4a0566e9c7b
Illia Shevchenko <illyashevchenko@gmail.com>
2018-03-25 14:41:07 +0300

    (client) Add calendar and a service to work with date ranges

---
+++ client/components/calendar/calendar.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export const Calendar = () => (
+  <div>
+    Here will be a calendar
+  </div>
+);
---
+++ client/components/calendar/dates.service.js
@@ -0,0 +1,13 @@
+const transformDate = (date) => date.toISOString().slice(0, 10);
+
+export const getDates = (dateFrom, dateTo) => {
+  const date = new Date(dateFrom);
+  const result = [];
+
+  while (date <= dateTo) {
+    result.push(transformDate(date));
+    date.setDate(date.getDate() + 1);
+  }
+
+  return result;
+};
