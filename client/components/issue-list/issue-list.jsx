36354408c651b45fbb566b2f4ec892358a8b853a
Illia Shevchenko <illyashevchenko@gmail.com>
2018-04-03 21:26:48 +0300

    (client) Improve issue list

--- client/components/issue-list/issue-list.jsx
+++ client/components/issue-list/issue-list.jsx
@@ -1,25 +1,61 @@
 import React from 'react';
-import { ListGroup, ListGroupItem } from 'react-bootstrap';
+import { toPairs } from 'ramda';
+
+import { ListGroup, ListGroupItem, Panel } from 'react-bootstrap';
 import { getHours } from '../calendar/dates.service';
 
-export const IssueList = ({ items, name, date }) =>
-  items && (
-    <div>
-      <h5>
-        { `${ name } on ${ date } was working on issues:` }
-      </h5>
-      <ListGroup>
-        { items.map(({ key, link, summary, timeSpentSeconds }, index) => (
-          <ListGroupItem
-            header={ `${ key }: ${ getHours(timeSpentSeconds) }h` }
-            href={ `https://${ link }` }
-            key={ key + index }
-            target="_blank"
-          >
-            { summary }
-          </ListGroupItem>
-        )) }
+import './issue-list.css';
 
-      </ListGroup>
-    </div>
-  );
+const renderTitle = ({ name, isGroup }) => (
+  isGroup
+    ? <h5>{ `Group ${ name }:` }</h5>
+    : null
+);
+
+const renderIssues = (issues) => (
+  <ListGroup>
+    { issues.map((
+      { key, link, summary, timeSpentSeconds },
+      index
+    ) => (
+      <ListGroupItem
+        header={ `${ key }: ${ getHours(timeSpentSeconds) }h` }
+        href={ `https://${ link }` }
+        key={ key + index }
+        target="_blank"
+      >
+        { summary }
+      </ListGroupItem>
+    )) }
+  </ListGroup>
+);
+
+const renderWorkLog = ([date, { issues }]) => (
+  <React.Fragment key={ date }>
+    <Panel.Body>{ `${ date }: ` }</Panel.Body>
+    { renderIssues(issues) }
+  </React.Fragment>
+);
+
+const renderUser = ([user, worklog]) => (
+  <Panel key={ user }>
+    <Panel.Heading>{ `${ user }:` }</Panel.Heading>
+    { toPairs(worklog).map(renderWorkLog) }
+  </Panel>
+);
+
+const renderUsers = (users) => (
+  users
+    ? (
+      <div>
+        { toPairs(users).map(renderUser) }
+      </div>
+    ) : null
+);
+
+export const IssueList = ({ users, name, isGroup }) => (
+  <div className="jw-issue-list">
+    { renderTitle({ name, isGroup }) }
+    { renderUsers(users) }
+  </div>
+);
--- client/containers/log-work/issue-list.js
+++ client/containers/log-work/issue-list.js
@@ -1,29 +1,40 @@
 import { connect } from 'react-redux';
 import { select } from '@rematch/select';
-import { path } from 'ramda';
+import { pick, map } from 'ramda';
 
 import { IssueList as IssueListComponent } from '../../components/issue-list';
 
-const selectUserIssues = (state, { name, date }) => path([name, date, 'issues'], select.users.worklog(state)) || [];
-const selectGroupIssues = () => [];
+const selectUserIssues = (state, { name }) => pick([name], select.users.worklog(state)) || {};
+const selectGroupIssues = (state, { name }) => {
+  const { users } = select.groups.byName(state, name);
+  return pick(users, select.users.worklog(state)) || {};
+};
 
-const selectIssues = (state) => {
-  const { isGroup, date, name } = select.issues.get(state) || {};
-
-  if (!name) {
-    return null;
-  }
-
+const getUsers = (state, { isGroup, date, name }) => {
   const selector = isGroup ? selectGroupIssues : selectUserIssues;
   return selector(state, { date, name });
 };
 
-const mapProps = (state) =>
-  Object.assign(
-    {
-      items: selectIssues(state),
-    },
-    select.issues.get(state),
-  );
+const getOnDates = (state, { date }, users) => {
+  const days = date ? [date] : select.calendar.days(state);
+
+  return map(pick(days), users);
+};
+
+const mapProps = (state) => {
+  const { isGroup, date, name } = select.issues.get(state) || {};
+
+  if (!name) {
+    return {};
+  }
+
+  const users = getUsers(state, { isGroup, date, name });
+
+  return {
+    users: getOnDates(state, { date }, users),
+    name: isGroup ? name : null,
+    isGroup,
+  };
+};
 
 export const IssueList = connect(mapProps)(IssueListComponent);
